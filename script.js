8c910269dab598b24857aefe7b1c30855388e5c7
Mario Heriberto <mlarios25@ucol.mx>
2026-02-05 16:18:08 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function cambiarcolor(){
+    document.body.style.backgroundColor="lightblue";
+}
+function cambiartexto(){
+    document.getElementById("texto").innerHTML="El texto ha cambiado";
+}
